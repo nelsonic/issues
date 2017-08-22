b8b5a0ee6c66f34fe7d5fd3c285d6e3fabe4ca8f
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-22 11:24:14 +0100

    adds tests for table formatter note: whitespace is expected!! see: https://stackoverflow.com/questions/30403150/atom-text-editor-remove-trailing-whitespace-on-save

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,6 +2,8 @@ defmodule Issues.CLI do
 
   @default_count 4
 
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @moduledoc """
   Handle the command line parsing and the dispatch to
   the various functions that end up generating a
@@ -42,10 +44,13 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
       |> decode_response
-      # |> sort_into_ascending_order
+      |> sort_into_ascending_order
+      |> Enum.take(count)
+      |> IO.inspect
+      |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
@@ -54,9 +59,9 @@ defmodule Issues.CLI do
     System.halt(2)
   end
 
-  # def sort_into_ascending_order(list_of_issues) do
-  #   Enum.sort list_of_issues,
-  #             fn i1, i2 -> Map.get(i1, "created_at") <= Map.get(i2, "created_at") end
-  # end
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> Map.get(i1, "created_at") <= Map.get(i2, "created_at") end
+  end
 
 end
