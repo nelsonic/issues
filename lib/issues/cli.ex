7c2fabfd3278e6d37b23ffc35865f182cc72fe1d
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-22 11:29:31 +0100

    adds escript to build command line application for https://github.com/nelsonic/practice/issues/12

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -10,7 +10,7 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
       |> parse_args
       |> process
--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.Mixfile do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
@@ -24,5 +25,9 @@ defmodule Issues.Mixfile do
       { :httpoison, "~> 0.9" },
       { :poison,    "~> 2.2" }
     ]
+  end
+  
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
