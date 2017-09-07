ab67b7fb64a4bd774f2405037c6c19ba757cd5f8
nelsonic <contact.nelsonic+github@gmail.com>
2017-09-07 17:03:56 +0100

    remove warnings by invoking functions in test file

--- test/issues_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule IssuesTest do
-  use ExUnit.Case
-  doctest Issues
-
-  test "greets the world" do
-    assert Issues.hello() == :world
-  end
-end
--- test/table_formatter_test.exs
+++ test/table_formatter_test.exs
@@ -14,17 +14,17 @@ defmodule TableFormatterTest do
   def headers, do: [ :c1, :c2, :c4 ]
 
   def split_with_three_columns,
-    do: TF.split_into_columns(simple_test_data, headers)
+    do: TF.split_into_columns(simple_test_data(), headers())
 
   test "split_into_columns" do
-    columns = split_with_three_columns
-    assert     length(columns) == length(headers)
+    columns = split_with_three_columns()
+    assert length(columns) == length(headers())
     assert List.first(columns) == ["r1 c1", "r2 c1", "r3 c1", "r4 c1"]
-    assert  List.last(columns) == ["r1+++c4", "r2 c4", "r3 c4", "r4 c4"]
+    assert List.last(columns) == ["r1+++c4", "r2 c4", "r3 c4", "r4 c4"]
   end
 
   test "column_widths" do
-    widths = TF.widths_of(split_with_three_columns)
+    widths = TF.widths_of(split_with_three_columns())
     assert widths == [ 5, 6, 7 ]
   end
 
@@ -35,7 +35,7 @@ defmodule TableFormatterTest do
 
   test "Output is correct" do
     result = capture_io fn ->
-      TF.print_table_for_columns(simple_test_data, headers)
+      TF.print_table_for_columns(simple_test_data(), headers())
     end
     assert result == """
     c1    | c2     | c4     
